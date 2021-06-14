3aae93deb907848918a2694d40d17d110d5ab5b1
Jacob Stewart <jacob.g.stewart@gmail.com>
2021-06-14 10:03:23 -0400

    finishing form validation logic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('form');
 const textField = document.querySelector('.text-field');
+const errorText = document.querySelector('.error-text');
 
 function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -11,7 +12,18 @@ form.addEventListener('submit', e => {
   if (validateEmail(textField.value)) {
     alert("You're on the list!"); 
   } else {
-    console.log('bad');
     textField.classList.add('text-field-error');
+    errorText.style.display = 'block';
   }
 });
+
+textField.addEventListener('input', () => {
+  console.log('changing');
+  if (validateEmail(textField.value)) {
+    textField.classList.add('text-field-verified');
+    textField.classList.remove('text-field-error');
+    errorText.style.display = 'none';
+  } else {
+    textField.classList.remove('text-field-verified');
+  }
+})
